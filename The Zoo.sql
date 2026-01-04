decde601db382a39f67f48c42a936f6242aa80ba
EstyWalter <estywalter4@gmail.com>
2026-01-04 16:15:43 -0500

    added spec

---
+++ The Zoo.sql
@@ -0,0 +1,80 @@
+ 
+
+
+
+/*
+I am a zookeeper. I take care of the animals. I feed them and make sure their happy, but it is pretty complicated to make sure that every animal gets their specific food 
+that they like and the perfect amounts. It is hard for me to keep track when to feed the animals and how much each animal has to eat.  
+We would like to record all these information.
+
+
+
+The cage number, the animal type, name of animal, the day they were born, the age of the animals, meals, treats,  Pounds per day, time of day that meal is given, 
+date they got their treats,  how often do they have to fast, date they last fasted
+
+
+
+1, Gorilla, Alex, 1999-05-09, Potatoes, apples & bananas, 20, Dawn-sunrise-morning-noon-afternoon-sunset, 2025-05-09,  no need,
+
+2, Tiger, Nala, 2024-08-09, horse meat, bloodsicles,  15,  Dawn-morning-afternoon-sunset-dusk-night , 2026-01-05, 1-7 days, 2026-01-04
+
+3, Lion, Leo, 2025-09-07, raw beef, bloodsicles,  12, Dawn-morning-afternoon-sunset-dusk-night-midnight, 2026-01-05, 1-23 days, 2026-01-04
+
+4, Kangaroo, Mbali, 2020-09-09, Hay, carrots, 20, morning,  2025-09-09, no need
+
+4, Kangaroo, Harrison, 2018-08-05, Hay, carrots,  22, morning, 2025-08-05 no need
+
+5, polar bear, Syanii, 2019-01-05, fish, ice cubes, 15, sunrise, 2025-01-05,  1-14 days, 2026-01-02
+
+6, panda bear, Ada, 2000-09-02,  bamboo, apples & carrots, 70, sunrise-morning-afternoon-sunset-evening-night, 2025-09-02, no need
+
+7, zebra, Zulu, 2009-12-05, grass hay, sugar canes, 7, sunrise-afternoon,  2025-12-05, no need
+.
+.
+What is the longest an animal can live in captivity?
+
+250 years is the longest that an animal could live in captivity.
+
+
+
+Are there more then one animal type in each cage?
+
+No we do not keep more than one animal type in each cage but from the same animal type their can be up to 10 animals in one cage.
+
+
+
+Do they eat more food as they become older?
+
+Sure they do, with every year they grow older they eat a pound more per day.
+
+
+
+how often do the animals get their treats ?
+
+On their birthdays and if the animals fast a day after their fast day.
+
+
+
+Can one animal have the same name as an other animal?
+
+Never, even animals from the same animal type have different names.
+
+
+
+what time of the day do you feed your animals?
+
+I feed them only these times of the day. Dawn, Sunrise, morning, Noon, afternoon, sunset, evening, Dusk, Night and midnight.
+
+
+
+reports
+
+what is the date of the next time the bear has to fast?
+
+I need to know how many animals their are per cage?
+
+how much do the animals have to eat per serving?
+*/
+
+
+
